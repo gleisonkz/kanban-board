93bd5a279360e18f65720ea221314686aea87dcf
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-12-31 18:01:06 -0300

    ✨ feat: components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'kanban-board';
+  todos: Task[] = [
+    { title: 'Aprender Angular' },
+    { title: 'Aprender React' },
+    { title: 'Aprender Vue' },
+  ];
+
+  inProgress: Task[] = [
+    { title: 'Aprender Type Script' },
+    { title: 'Aprender SCSS' },
+    { title: 'Aprender WEbPack' },
+  ];
+
+  completed: Task[] = [
+    { title: 'Aprender HTML' },
+    { title: 'Aprender CSS' },
+    { title: 'Aprender Java Script' },
+  ];
+
+  todosTest: string[] = ['Aprender Angular', 'Aprender React', 'Aprender Vue'];
+
+  inProgressTest: string[] = [
+    'Aprender Type Script',
+    'Aprender SCSS',
+    'Aprender WEbPack',
+  ];
+
+  completedTeste: string[] = [
+    'Aprender HTML',
+    'Aprender CSS',
+    'Aprender Java Script',
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
---
+++ src/app/components/task-board/task-board.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task.model';
+
+@Component({
+  selector: 'app-task-board',
+  templateUrl: './task-board.component.html',
+  styleUrls: ['./task-board.component.scss'],
+})
+export class TaskBoardComponent implements OnInit {
+  @Input() title: string = '';
+  @Input() iconName: string = '';
+  @Input() taskList: Task[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
